bbc3e9800e8daf5044ea0527371078eca08359b0
raphael meyer <raphael.akira.meyer@gmail.com>
2019-09-15 10:13:31 +0200

    working on slides

---
+++ examples/haskell/either.hs
@@ -0,0 +1,30 @@
+data Volume = Volume Int deriving (Show)
+data Moisture = Moisture Int deriving (Show)
+data Temperature = Temperature Float deriving (Show)
+
+data Error = Error String deriving (Show)
+
+readMoisture :: Either Error Moisture
+readMoisture = Right (Moisture 40)
+
+readTemperature :: Either Error Temperature
+-- readTemperature = Right (Temperature 21.5)
+readTemperature = Left (Error "Temperature sensor offline")
+
+calculateAmount :: Moisture -> Temperature -> Either Error Volume
+calculateAmount _ _ = Right (Volume 123)
+
+pump :: Volume -> Either Error ()
+pump _ = Right ()
+
+water :: Either Error Volume
+water = do
+  moisture <- readMoisture
+  temperature <- readTemperature
+  amount <- calculateAmount moisture temperature
+  pump amount
+  return amount
+
+main = do
+  let amount = water
+  either (putStrLn . ("! " ++) . show) (putStrLn . show) amount
