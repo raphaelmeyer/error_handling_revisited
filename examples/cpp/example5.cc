7ef3a9764593dfadce444ddb6e2497d3af45b7b8
raphael meyer <raphael.akira.meyer@gmail.com>
2019-10-22 18:09:38 +0200

    improve consistency for example 5

--- examples/cpp/example5.cc
+++ examples/cpp/example5.cc
@@ -1,17 +1,24 @@
+#include <iostream>
 #include <optional>
 
-class Volume {};
-class Moisture {};
-class Temperature {};
+struct Volume { int ml; };
+struct Moisture { int percentage; };
+struct Temperature { double celsius; };
 
 class ThermoSensor {
 public:
-  std::optional<Temperature> read() { return Temperature{}; }
+  std::optional<Temperature> read() {
+    // return std::nullopt;
+    return Temperature{21.5};
+  }
 };
 
 class MoistureSensor {
 public:
-  std::optional<Moisture> read() { return Moisture{}; }
+  std::optional<Moisture> read() {
+    // return std::nullopt;
+    return Moisture{40};
+  }
 };
 
 class Pump {
@@ -31,15 +38,18 @@ public:
       return std::nullopt;
     }
     auto const amount = calculate_amount(moisture.value(), temperature.value());
-    if (not pump.pump(amount)) {
+    if (not amount) {
+      return std::nullopt;
+    }
+    if (not pump.pump(amount.value())) {
       return std::nullopt;
     }
     return amount;
   }
 
 private:
-  Volume calculate_amount(Moisture moisture, Temperature temperature) {
-    return {};
+  std::optional<Volume> calculate_amount(Moisture moisture, Temperature temperature) {
+    return Volume{178};
   }
 
   MoistureSensor moisture_sensor;
@@ -48,6 +58,10 @@ private:
 };
 
 int main() {
-  WateringSystem system;
-  system.water();
+  auto const amount = WateringSystem{}.water();
+  if (amount) {
+    std::cout << "Water " << amount.value().ml << " ml\n";
+  } else {
+    std::cout << "Watering error\n";
+  }
 }
